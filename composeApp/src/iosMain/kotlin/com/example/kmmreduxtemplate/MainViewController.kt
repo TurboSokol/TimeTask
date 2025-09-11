9399b56daca8ecf3d0d9910887eff7d8cd48f97c
turbosokol <saintpapasha@gmail.com>
2025-09-11 13:35:03 +0400

    fix ios run

--- composeApp/src/iosMain/kotlin/com/example/kmmreduxtemplate/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/kmmreduxtemplate/MainViewController.kt
@@ -5,11 +5,17 @@ import com.example.kmmreduxtemplate.di.initKoinForCompose
 
 /***
  *If this code runs it was created by Evgenii Sokol.
- *If it doesn’t work, I don’t know who was created it.
+ *If it doesn't work, I don't know who was created it.
  ***/
 
-fun MainViewController() = ComposeUIViewController { 
-    // Initialize Koin for iOS
+// Initialize Koin once at module level for iOS
+private val isKoinInitialized by lazy {
     initKoinForCompose()
+    true
+}
+
+fun MainViewController() = ComposeUIViewController { 
+    // Ensure Koin is initialized before UI
+    isKoinInitialized
     MainScreenWithBottomNavBar()
 }
